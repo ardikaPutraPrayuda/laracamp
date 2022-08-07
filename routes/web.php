df1b75fb75adae3596fcca432a7c2994f0adb70c
ardikaPutraPrayuda <ardikaputraa22@gmail.com>
2022-08-07 21:17:07 +0700

    checkout part 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,17 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-// Route::get('/login', function () {
-//     return view('login');
-// })->name('login');
 
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name('checkout');
+Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('/checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
 
-Route::get('/success_checkout', function () {
-    return view('success_checkout');
-})->name('success_checkout');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
